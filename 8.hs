41affd801642ce019e76d015bd9cfc1038ac7ff9
Ben Wiklund <ben@daisyowl.com>
2015-03-05 20:05:33 -0800

    8

---
+++ 8.hs
@@ -0,0 +1,12 @@
+import System.IO
+
+-- load the file, strip newlines, convert to ints
+main = readFile "8.txt" >>= print . problem8 . stringToInts . concat . lines
+
+stringToInts = map ((read :: String -> Int) . (:[]))
+
+-- the actual problem
+problem8 xs = maximum $ productWindows 13 xs
+
+productWindows _ [] = []
+productWindows width xs = (product $ take width xs) : (productWindows width $ tail xs)
