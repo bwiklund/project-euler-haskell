fd0f5e4631a81ed6f714044622e4452bed6d11df
Ben Wiklund <ben@daisyowl.com>
2015-03-06 17:29:31 -0800

    17

--- 17.hs
+++ 17.hs
@@ -1,24 +1,36 @@
 import Data.Array
 
-main = print $ problem17 [1..99]
+main = print $ problem17 [1..1000]
 
 -- rules relevant ways of spelling numbers from 1-1000
 -- 1-9 -> unique
 -- 20-99 -> TENS + 1-9 rules
 -- 100-999 -> X hundred (and XX)
-
-sayNumber :: Int -> String
-sayNumber n
-  | n < 20 = oneToNineteen n
-  | n < 100 = twentyToNinetyNine n
-  | otherwise = "er"
+-- no spaces or hyphens are counted
 
 oneToNineteen n = (listArray (0,19) ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]) ! n
 
 tensPrefixes n = (listArray (2,9) ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]) ! n
 
+sayNumber :: Int -> String
+sayNumber n
+  | n < 100 = oneToNinetyNine n
+  | n < 1000 = hundreds n
+  | n == 1000 = "onethousand" -- lazy
+  | otherwise = "er"
+
+oneToNinetyNine n
+  | n < 20 = oneToNineteen n
+  | n < 1000 = twentyToNinetyNine n
+  | otherwise = "uh"
+
+hundreds n =
+  let (hundos, leftovers) = n `quotRem` 100
+  in (oneToNineteen hundos) ++ "hundred" ++
+    if leftovers /= 0 then "and" ++ oneToNinetyNine leftovers else ""
+
 twentyToNinetyNine n =
   let tens = n `quot` 10
   in tensPrefixes tens ++ oneToNineteen (n - tens * 10)
 
-problem17 xs = map sayNumber xs
+problem17 xs = length $ concatMap sayNumber xs
