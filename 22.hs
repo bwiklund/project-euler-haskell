268d7d9eb98c205deca54023a81c45f3fb386018
Ben Wiklund <ben@daisyowl.com>
2015-03-08 23:33:54 -0700

    parsing 22.txt

---
+++ 22.hs
@@ -0,0 +1,7 @@
+import System.IO
+import Data.List.Split
+
+main = readFile "22.txt" >>= print . problem22 . formatInput
+  where formatInput = map (tail . init) . splitOn "," . filter (/= '\n')
+
+problem22 = id
