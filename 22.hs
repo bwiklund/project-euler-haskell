b4728beb360a908c49cad19ec7bed94f04bedc4c
Ben Wiklund <ben@daisyowl.com>
2015-03-20 20:52:23 -0700

    bleh

--- 22.hs
+++ 22.hs
@@ -1,18 +1,7 @@
 import System.IO
 import Data.List.Split
-import Data.Char
-import Data.List
 
 main = readFile "22.txt" >>= print . problem22 . formatInput
   where formatInput = map (tail . init) . splitOn "," . filter (/= '\n')
 
-problem22 :: [String] -> Int
-problem22 strs = sum scores
-  where scores = map score (zip (sort strs) [1..])
-        
-
-score :: (String, Int) -> Int
-score (str, rank) = rank * (sum $ map charToPosition str)
-
-charToPosition :: Char -> Int
-charToPosition = (+ (-96)) . ord . toLower
+problem22 =
--- 23.hs
+++ 23.hs
@@ -1,5 +1,9 @@
 main = print problem23
 
-problem23 = factors 10
+problem23 = nonAbundantNumbers
+
+nonAbundantNumbers = filter (not . isAbundant) [1..28123]
 
 factors n = [x | x <- [1..(n `quot` 2)], n `rem` x == 0]
+
+isAbundant n = sum (factors n) > n
