b3e208ee1eaf8111429ba27b318633ea0034e5be
Ben Wiklund <ben@daisyowl.com>
2015-03-22 21:17:36 -0700

    30 bounded

--- 30.hs
+++ 30.hs
@@ -1,8 +1,14 @@
 main = print $ problem30 5
 
-problem30 exp = scanl1 (+) $ allSumOfDigitPowers exp
+problem30 exp = sum $ allSumOfDigitPowers exp
 
-allSumOfDigitPowers exp = filter (isSumOfDigitPowers exp) [10..]
+allSumOfDigitPowers exp = filter (isSumOfDigitPowers exp) [10..upperBoundForExp exp]
+
+-- at some point, the number of digits will be large enough that `9^exp * numDigits` won't be large enough create a number of that many digits. So that's a quick simple way to find an upper bound.
+-- i could do this with algebra, or just quick and dirty
+upperBoundForExp exp = 10 ^ boundExp
+  where boundExp = length $ takeWhile isPossible [1..]
+        isPossible = \x -> x * 9^exp > 10 ^ (x-1)
 
 isSumOfDigitPowers exp x =
   let digits = map charDigitToInt (show x)
